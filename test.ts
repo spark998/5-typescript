72542cbbf8c9eb338cf53565171f314e469a0c48
Yuriy <example@gmail.com>
2023-01-09 17:37:23 +0300

    -Типизация объекта

---
+++ test.ts
@@ -0,0 +1,21 @@
+let info: {
+	officeId: number;
+	isOpened: boolean;
+	contacts: {
+		phone: number;
+		email: string;
+		address: {
+			city: string;
+		}
+	}
+} = {
+	"officeId": 45,
+	"isOpened": false,
+	"contacts": {
+		"phone": 34334343535,
+		"email": "ny@email.com",
+		"address": {
+			"city": "Москва"
+		}
+	}
+}
