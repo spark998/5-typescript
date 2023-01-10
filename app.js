06b025ff88b9277de5f25199b4df39e713ed0ddc
Yuriy <example@gmail.com>
2023-01-10 12:49:38 +0300

    Enum

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 "use strict";
-const skill = [1, 'Dev'];
-const skills = ['Dev', 'DevOps'];
-// skills[0] = '';
+/*enum StatusCode {
+    SUCCESS,
+    IN_PROCESS,
+    FAILED
+}*/
+/*Числовой enum
+enum StatusCode {
+    SUCCESS = 1,
+    IN_PROCESS = 2,
+    FAILED = 4
+}*/
+/*Cтроковый enum
+enum StatusCode {
+    SUCCESS = 's',
+    IN_PROCESS = 'p',
+    FAILED = 'f'
+}*/
+//Гетерогенные enum
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode["IN_PROCESS"] = "p";
+    StatusCode["FAILED"] = "f";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: 'Платёж успешен',
+    statusCode: StatusCode.SUCCESS
+};
+function action(status) {
+}
+action(StatusCode.SUCCESS);
+action(1);
+const res2 = 1 /* Roles.ADMIN */;
--- app.ts
+++ app.ts
@@ -1,4 +1,58 @@
-const skill: readonly [number, string] = [1, 'Dev'];
+/*enum StatusCode {
+	SUCCESS,
+	IN_PROCESS,
+	FAILED
+}*/
 
-const skills: readonly string[] = ['Dev', 'DevOps'];
-// skills[0] = '';
+/*Числовой enum
+enum StatusCode {
+	SUCCESS = 1,
+	IN_PROCESS = 2,
+	FAILED = 4
+}*/
+
+/*Cтроковый enum
+enum StatusCode {
+	SUCCESS = 's',
+	IN_PROCESS = 'p',
+	FAILED = 'f'
+}*/
+
+//Гетерогенные enum
+enum StatusCode {
+	SUCCESS = 1,
+	IN_PROCESS = 'p',
+	FAILED = 'f'
+}
+
+
+
+const res = {
+	message: 'Платёж успешен',
+	statusCode: StatusCode.SUCCESS
+};
+
+function action(status: StatusCode) {
+
+}
+
+action(StatusCode.SUCCESS);
+action(1);
+//action('p');
+
+/*enum Roles {
+	ADMIN = 1,
+	USER = 2;
+}*/
+
+/*function test(x: {ADMIN: number}) {
+
+}
+test(Roles);*/
+
+const enum Roles {
+	ADMIN = 1,
+	USER = 2
+}
+
+const res2 = Roles.ADMIN;
