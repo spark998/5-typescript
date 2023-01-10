e755c7feb3b5bc94fb8ccb73a092fa9a2d8fc582
Yuriy <example@gmail.com>
2023-01-10 12:09:38 +0300

    Readonly

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 "use strict";
 const skill = [1, 'Dev'];
-//const id = skill[0];
-//const skillName = skill[1];
-//skill.push('afavcav');
-//const sad = skill[2];
-const [id, skillName] = skill;
-//типизация массива произвольной длинны
-const arr = [1, 'dafcav', true, true, false];
+const skills = ['Dev', 'DevOps'];
+// skills[0] = '';
--- app.ts
+++ app.ts
@@ -1,9 +1,4 @@
-const skill: [number, string] = [1, 'Dev'];
+const skill: readonly [number, string] = [1, 'Dev'];
 
-//const id = skill[0];
-//const skillName = skill[1];
-//skill.push('afavcav');
-//const sad = skill[2];
-const [id, skillName] = skill;
-//типизация массива произвольной длинны
-const arr: [number, string, ...boolean[]] = [1, 'dafcav', true, true, false];
+const skills: readonly string[] = ['Dev', 'DevOps'];
+// skills[0] = '';
