adfdbc958273a8ace188a878925c04794c8e2fec
Yuriy <example@gmail.com>
2023-01-12 21:58:23 +0300

    -Типизация ответа сервера

--- app.ts
+++ app.ts
@@ -1,35 +1,3 @@
-interface User {
-	login: string,
-	password?: string
-}
-
-const user: User = {
-	login: 'fsfs@da',
-	password: '1312'
-}
-
-function multiply(first: number, second?: number): number {
-	if (!second) {
-		return first * first;
-	}
-	return first * second;
-}
-multiply(5);
-
-interface User1 {
-	login: string,
-	password?: {
-		type: 'primory' | 'secondary'
-	}
-}
-function testPass(user: User1) {
-	const t = user.password?.type;
-}
-
-function test(param?: string) {
-	const t = param ?? multiply(5);
-}
-
 interface IPayment {
 	sum: number,
 	from: number,
@@ -62,6 +30,6 @@ interface IResponseFailed {
 	data: IDataFailed
 }
 
-function get(): IResponseSuccess | IResponseFailed {
+/*function get(): IResponseSuccess | IResponseFailed {
 	
-}
+}*/
