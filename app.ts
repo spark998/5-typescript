8664e15ba5b004202d4ae96d6046e74d64d197d4
Yuriy <example@gmail.com>
2023-01-12 21:47:07 +0300

    -Типизация ответа сервера

--- app.ts
+++ app.ts
@@ -29,3 +29,39 @@ function testPass(user: User1) {
 function test(param?: string) {
 	const t = param ?? multiply(5);
 }
+
+interface IPayment {
+	sum: number,
+	from: number,
+	to: number
+}
+interface IPaymentRequest extends IPayment {}
+
+enum PaymentStatus {
+	Succes = 'success',
+	Failed = 'failed'
+}
+
+interface IDataSuccess extends IPayment {
+	databaseId: number
+}
+
+interface IDataFailed {
+	errorMessage: string,
+	errorCode: number
+}
+
+interface IResponseSuccess {
+	status: PaymentStatus.Succes,
+	data: IDataSuccess	
+	
+}
+
+interface IResponseFailed {
+	status: PaymentStatus.Failed,
+	data: IDataFailed
+}
+
+function get(): IResponseSuccess | IResponseFailed {
+	
+}
